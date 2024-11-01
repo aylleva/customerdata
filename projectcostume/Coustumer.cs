2c296ce311ae6ba66f21c922d69b620e800304ff
ayleva <ayllayeva@gmail.com>
2024-11-01 10:31:52 +0400

    initial commit

---
+++ projectcostume/Coustumer.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectcostume
+{
+    
+           
+    internal class Cuostumer
+    {
+        public static string path = @"C:\Users\user\Desktop\projectcostume\projectcostume\Data\costumers.json";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+
+
+        public static List<Cuostumer> Deserialize(string path)
+        {
+            string result;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                result = sr.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<Cuostumer>>(result);
+        }
+        public static void Serialize(List<Cuostumer> costumers)
+        {
+            string result = JsonConvert.SerializeObject(costumers);
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine(result);
+            }
+        }
+
+        public static void Add(Cuostumer costumer)
+        {
+            List<Cuostumer>customers  = Deserialize(path);
+            customers.Add(costumer);
+
+            Serialize(customers);
+        }
+
+        public static void CustomerSearch(int id)
+        {
+            List<Cuostumer> customers = Deserialize(path).FindAll(f => f.Id == id);
+            customers.ForEach(find => Console.WriteLine(find.Name + " " + find.Surname + " id:" + find.Id));
+
+
+        }
+
+        public static void Update(int id, string newName, string newSurname, string newPhoneNumber)
+        {
+            List<Cuostumer> customers = Deserialize(path);
+            Cuostumer newcustomer = customers.Find(n => n.Id == id);
+
+           newcustomer.Id=id;
+           newcustomer.Name = newName;
+           newcustomer.Surname=newSurname;
+           newcustomer.PhoneNumber = newPhoneNumber;
+            
+            Serialize(customers);
+        }
+
+        public static void DeleteCostumer(int id)
+        {
+            List<Cuostumer> costumers = Deserialize(path);
+            costumers.RemoveAll(f => f.Id == id);
+          
+            Serialize(costumers);
+        }
+
+        public static void GettAll()
+        {
+            List<Cuostumer> costumers = Deserialize(path);
+            costumers.ForEach(find => Console.WriteLine(find.Id + " " + find.Name + " " + find.Surname + " "+find.PhoneNumber));
+        }
+    }
+}
---
+++ projectcostume/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Nodes;
+
+namespace projectcostume
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+
+
+            // Directory.CreateDirectory(@"C:\Users\user\Desktop\projectcostume\projectcostume\Data");
+            //File.Create(@"C:\Users\user\Desktop\projectcostume\projectcostume\Data\costumers.json");
+
+            List<Cuostumer> customers = new List<Cuostumer>
+            {
+                new Cuostumer{Id=1,Name="Samurai",Surname="Samuraiova",PhoneNumber="+9946664433"},
+                new Cuostumer{Id=2,Name="True",Surname="Man",PhoneNumber="+9941234567"},
+                new Cuostumer{Id=3,Name="Relax",Surname="Relaxova",PhoneNumber="+994348282"}
+            };
+
+            Cuostumer.Serialize(customers);
+            Cuostumer.Add(new Cuostumer { Id = 4, Name = "Ayla", Surname = "Atakisiyeva", PhoneNumber = "+9943492543" });
+
+            Cuostumer.CustomerSearch(2);
+
+            Cuostumer.DeleteCostumer(2);
+
+            Cuostumer.Update(4, "Kobra", "Kobraova", "+9944593992");
+
+            Cuostumer.GettAll();
+        }
+       
+
+
+
+
+    }
+}
